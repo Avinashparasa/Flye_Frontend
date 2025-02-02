7efdefa2981ca104f019545ce520fb7daa84dd8b
AvinashParasa <parasaavinash27@gmail.com>
2025-02-02 12:25:53 +0530

    updated

--- src/components/app.routes.ts
+++ src/components/app.routes.ts
@@ -4,12 +4,7 @@ import { WorkoutListComponent } from './workout-list/workout-list.component';
 import { WorkoutChartComponent } from './workout-chart/workout-chart.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'workout-form',
-    component: WorkoutFormComponent,
-    pathMatch: 'full'
-  },
+  
   {
     path: 'work-chart',
     // redirectTo: 'workout-form',
